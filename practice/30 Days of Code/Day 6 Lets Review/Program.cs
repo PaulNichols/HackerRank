f74cdc54f682b808215a18fc02cd59028cd3b487
Paul Nichols <automation@mexia.com.au>
2020-05-21 18:45:35 +1000

    added cs day 6

---
+++ practice/30 Days of Code/Day 6 Lets Review/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+        var size = int.Parse(Console.ReadLine());
+        var strings = new List<string>();
+
+        for (int i = 0; i < size; i++)
+        {
+            strings.Add(Console.ReadLine());
+        }
+
+        var splitStrings = Split(strings);
+
+        foreach (var item in splitStrings)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    private static List<string> Split(List<string> strings)
+    {
+        var retrunStrings = new List<string>();
+
+        foreach (var item in strings)
+        {
+            var letters = item.ToCharArray();
+            var odds = "";
+            var evens = "";
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                var currentLetter = letters[i].ToString();
+
+                if (i % 2 == 0)
+                {
+                    evens += currentLetter;
+                }
+                else
+                {
+                    odds += currentLetter;
+                }
+            }
+
+            retrunStrings.Add($"{evens} {odds}");
+        }
+
+        return retrunStrings;
+    }
+}
