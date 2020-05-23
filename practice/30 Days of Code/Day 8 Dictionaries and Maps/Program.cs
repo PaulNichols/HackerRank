6ca24c98826d099e92fc2e17b7debc0434f95d84
Paul Nichols <automation@mexia.com.au>
2020-05-23 20:43:17 +1000

    Added cs solutions for 30 days of code day 8 & 9

---
+++ practice/30 Days of Code/Day 8 Dictionaries and Maps/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Solution
+{
+    private static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        var phoneBook = new Dictionary<string, string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] keyValue = Console.ReadLine().Split(' ');
+            phoneBook.Add(keyValue[0], keyValue[1]);
+        }
+
+        var queries = new List<string>();
+        string query;
+
+        do
+        {
+            query = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                queries.Add(query);
+            }
+        } while (!string.IsNullOrWhiteSpace(query));
+
+        for (int i = 0; i < queries.Count; i++)
+        {
+            if (phoneBook.ContainsKey(queries[i]))
+            {
+                Console.WriteLine($"{queries[i]}={phoneBook[queries[i]]}");
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+    }
+}
---
+++ practice/30 Days of Code/Day 9 Recursion 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+internal class Solution
+{
+    // Complete the factorial function below.
+    private static int factorial(int n)
+    {
+        var result = n;
+        for (int i = n - 1; i >= 1; i--)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    private static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = factorial(n);
+
+        Console.WriteLine(result);
+    }
+}
