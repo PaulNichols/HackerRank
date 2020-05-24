912f58d18652f648dc113fcb0d711ebe19ebbc45
Paul Nichols <automation@mexia.com.au>
2020-05-24 11:30:03 +1000

    added cs solution for 30 days of code day 10

---
+++ practice/30 Days of Code/Day 10 Binary Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class ConversionExample
+{
+    public static void Main(string[] args)
+    {
+        int n, i;
+        var binaryDigits = new List<int>();
+
+        n = int.Parse(Console.ReadLine());
+        for (i = 0; n > 0; i++)
+        {
+            binaryDigits.Add(n % 2);
+            n = n / 2;
+        }
+
+        string binaryString = "";
+
+        for (i = i - 1; i >= 0; i--)
+        {
+            binaryString += binaryDigits [i];
+        }
+
+      Console.WriteLine( binaryString.Split('0').Select(x=>x.Length).Max());
+    }
+}
