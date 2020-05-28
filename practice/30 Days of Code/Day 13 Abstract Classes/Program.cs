fd943be6fda97e38c03d33a6b4949c3f7b2a2216
Paul Nichols <automation@mexia.com.au>
2020-05-28 17:37:19 +1000

    added cs solution for day 13

---
+++ practice/30 Days of Code/Day 13 Abstract Classes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+internal abstract class Book
+{
+    protected String title;
+    protected String author;
+
+    public Book(String t, String a)
+    {
+        title = t;
+        author = a;
+    }
+
+    public abstract void display();
+}
+
+internal class Solution
+{
+    private static void Main(String[] args)
+    {
+        String title = Console.ReadLine();
+        String author = Console.ReadLine();
+        int price = Int32.Parse(Console.ReadLine());
+        Book new_novel = new MyBook(title, author, price);
+        new_novel.display();
+    }
+}
+
+internal class MyBook : Book
+{
+    private readonly int price;
+
+    public MyBook(string title, string author, int price) : base(title, author)
+    {
+        this.price = price;
+    }
+
+    public override void display()
+    {
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Author: {author}");
+        Console.WriteLine($"Price: {price}");
+    }
+}
