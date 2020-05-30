17358c76e79e0accd7f5f01d8419ee3e6324c7a4
Paul Nichols <automation@mexia.com.au>
2020-05-30 11:14:57 +1000

    added day 16 cs solution

---
+++ practice/30 Days of Code/Day 16 Exceptions - String to Integer/cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+internal class Solution
+{
+    private static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+
+        try
+        {
+            var integrer = int.Parse(s);
+            Console.WriteLine(integrer);
+        }
+        catch
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
