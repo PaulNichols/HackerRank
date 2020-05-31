8ef1612a176c8dea2c258c53f97cc9e389094f74
Paul Nichols <automation@mexia.com.au>
2020-05-31 18:29:58 +1000

    Data Structures Arrays - DS cs solution

---
+++ practice/Problem Solving/Data Structures/Arrays - DS/cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+internal class Solution
+{
+    // Complete the reverseArray function below.
+    private static int[] reverseArray(int[] a)
+    {
+        return a.Reverse().ToArray();
+    }
+
+    private static void Main(string[] args)
+    {
+        int arrCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int[] res = reverseArray(arr);
+
+        Console.WriteLine(string.Join(" ", res));
+    }
+}
