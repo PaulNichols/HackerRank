1d3dd9c89cdd25851606d06565e0bafb92728345
Paul Nichols <automation@mexia.com.au>
2020-06-06 12:09:06 +1000

    day 17 cs solution

---
+++ practice/30 Days of Code/Day 18 Queues and Stacks/cs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+internal class Solution
+{
+    //Write your code here
+
+    private static void Main(String[] args)
+    {
+        // read the string s.
+        string s = Console.ReadLine();
+
+        // create the Solution class object p.
+        Solution obj = new Solution();
+
+        // push/enqueue all the characters of string s to stack.
+        foreach (char c in s)
+        {
+            obj.pushCharacter(c);
+            obj.enqueueCharacter(c);
+        }
+
+        bool isPalindrome = true;
+
+        // pop the top character from stack.
+        // dequeue the first character from queue.
+        // compare both the characters.
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            if (obj.popCharacter() != obj.dequeueCharacter())
+            {
+                isPalindrome = false;
+
+                break;
+            }
+        }
+
+        // finally print whether string s is palindrome or not.
+        if (isPalindrome)
+        {
+            Console.Write("The word, {0}, is a palindrome.", s);
+        }
+        else
+        {
+            Console.Write("The word, {0}, is not a palindrome.", s);
+        }
+    }
+
+    private object dequeueCharacter()
+    {
+        throw new NotImplementedException();
+    }
+
+    private object popCharacter()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void pushCharacter(char c)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void enqueueCharacter(char c)
+    {
+        throw new NotImplementedException();
+    }
+}
